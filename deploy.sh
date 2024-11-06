d64230d88247ded0c4b010eeed2bad49aeadecd6
yang <Yhmecnu@163.com>
2024-11-06 14:27:48 +0800

    change port

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 set -ex
 docker build -t cal-code .
-docker rm -f cal-code; docker run -d -p 9004:9004 \
+docker rm -f cal-code; docker run -d -p 5123:5123 \
     -v /home/cal_code:/cal_code \
     -v /opt/logs/vola:/cal_code/vola \
     --name=cal-code cal-code
