1ebccb12b61e46a3408842d43f5ef232490a8d79
yang <Yhmecnu@163.com>
2024-11-06 15:46:37 +0800

    debug

--- jy_test/deploy.sh
+++ jy_test/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 set -ex
-docker build -t cal-code -f jy_test/dockerfile
+docker build -t cal-code -f jy_test/dockerfile .
 docker rm -f cal-code; docker run -d --env-file .env \
     -v /home/cal_code/scripts:/app/cal_code \
     -v /opt/logs/vola:/app/vola \
